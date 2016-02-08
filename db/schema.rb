ebcd179b3c45e0ec34a808463b4a0e71dd004a8b
Miko Taruc <miko.taruc@gmail.com>
2016-02-07 21:53:46 -0500

    Recipes per user basis

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160208022139) do
+ActiveRecord::Schema.define(version: 20160208024500) do
 
   create_table "recipes", force: :cascade do |t|
     t.string   "name",        limit: 255
@@ -19,7 +19,10 @@ ActiveRecord::Schema.define(version: 20160208022139) do
     t.text     "ingredients", limit: 65535
     t.datetime "created_at",                null: false
     t.datetime "updated_at",                null: false
+    t.integer  "user_id",     limit: 4
   end
+
+  add_index "recipes", ["user_id"], name: "index_recipes_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -40,4 +43,5 @@ ActiveRecord::Schema.define(version: 20160208022139) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "recipes", "users"
 end
